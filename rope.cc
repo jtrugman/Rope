a2ff0999eaf9776ff33be5819ea639c0c20151d9
Justin Trugman <jtrugman14@gmail.com>
2019-04-22 13:03:35 -0400

    new file and concat

--- rope.cc
+++ rope.cc
@@ -1,36 +1,50 @@
-/*
-Authors: Justin Trugman, David Lehman, Kirk Vasilas
-
-Rope Project: We are using the Rope data structure in order to create an ultra high performance text editior
-*/
-using namespace std;
-
 class Rope {   // Rope data structure
 private:
 
-    class RopeNode {
-    private:
-        char data[];
+    class Node {
+    public:
+        char *data;
         int weight;
-        RopeNode* right;
-        RopeNode* left;
-    }
+        Node *left, *right;
+        Node(char *data) : data(data), weight(sizeof(data)/sizeof(char)), left(nullptr), right(nullptr){}
+        Node() : data(nullptr), weight(0), left(nullptr), right(nullptr){}
+    };
+
+    Node* root;
 
 public:
-    Rope() {}
-    Rope(const char str[]) {}
-    Rope(const char str[], int len) {}
+    Rope() : root(new Node()) {}
+
+    // Rope(const char str[]) {}
+    // Rope(const char str[], int len) {}
+
+    void makeEmpty () {
+        root = new Node();
+    }
+
+    void concat(char *str) {
+        Node *temp = new Node(str);
+        Node *newRoot = new Node();
+        newRoot->left = root;
+        newRoot->right = temp;
+        newRoot->weight = newRoot->left->weight;
+        if (newRoot->left->right != nullptr) {
+            newRoot->weight += newRoot->left->right->weight;
+        }
+        
+    }
 
     /*
         Returns value at index i 
     */
-    char index(const RopeNode& node, int i) {   // like cursor???
-        if (node.weight <= i) {
-            return index(node.right, i - node.weight);
-        } else if (node.left != nullptr) {
-            return index(node.left, i);
+    char index(Node* node, int i) {   // like cursor???
+        Node* p = node;
+        if (p->weight <= i) {
+            return index(p->right, i - p->weight);
+        } else if (p->left != nullptr) {
+            return index(p->left, i);
         }
-        return node.data[i];
+        return p->data[i];
     }
 
 
@@ -41,25 +55,5 @@ public:
     Cursor begin() const;
     Cursor end() const;
     Cursor get(int i) const {} // returns Cursor requested
-
-};
-
-class Cursor {  // Determines spot to insert, select
-public:
-    Cursor() {}
-    Cursor(int offset) {} 
-    Cursor(const Cursor& c, int offset) {}
-};
-
-class Range {   // Range of selection 
-private:
-    Cursor start, end;
-public:
-    Range(const Cursor& start, const Cursor& end) : start(start), end(end) {}
-    int length() const {}   // returns length of the range
-
-};
-
-class Window {  // window of text a user is viewing and able to edit
 
 };
